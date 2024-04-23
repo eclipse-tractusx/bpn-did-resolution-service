f4f1696384ee5ff6bceb4d8ee85f3315fa4e06a0
Paul Latzelsperger <43503240+paullatzelsperger@users.noreply.github.com>
2024-04-23 08:42:10 +0200

    feat: read DB secrets and API key from vault (#35)
    * feat: read db config and api key from vault
    
    * helm docs, lint
    
    * DEPENDENCIES
    
    * separate vault deployment in test
    
    * wait for ready pods
    
    * add hashicorp rep
    
    * change readiness condition
    
    * change wait condition again
    
    * added comment [skip ci]

--- extensions/store/sql/sql-lib/src/main/java/org/eclipse/tractusx/bdrs/sql/migration/AbstractPostgresqlMigrationExtension.java
+++ extensions/store/sql/sql-lib/src/main/java/org/eclipse/tractusx/bdrs/sql/migration/AbstractPostgresqlMigrationExtension.java
@@ -21,28 +21,40 @@
 package org.eclipse.tractusx.bdrs.sql.migration;
 
 import org.eclipse.edc.runtime.metamodel.annotation.Setting;
+import org.eclipse.edc.spi.EdcException;
+import org.eclipse.edc.spi.monitor.Monitor;
 import org.eclipse.edc.spi.persistence.EdcPersistenceException;
+import org.eclipse.edc.spi.security.Vault;
 import org.eclipse.edc.spi.system.ServiceExtension;
 import org.eclipse.edc.spi.system.ServiceExtensionContext;
+import org.eclipse.edc.spi.system.configuration.Config;
 import org.eclipse.edc.sql.DriverManagerConnectionFactory;
 import org.eclipse.edc.sql.datasource.ConnectionFactoryDataSource;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 import java.util.Properties;
+import java.util.function.Supplier;
 
+import static java.util.Optional.ofNullable;
 import static org.flywaydb.core.api.MigrationVersion.LATEST;
 
 abstract class AbstractPostgresqlMigrationExtension implements ServiceExtension {
 
     private static final String EDC_DATASOURCE_PREFIX = "edc.datasource";
-
     private static final String DEFAULT_MIGRATION_ENABLED_TEMPLATE = "true";
     @Setting(value = "Enable/disables subsystem schema migration", defaultValue = DEFAULT_MIGRATION_ENABLED_TEMPLATE, type = "boolean")
     private static final String MIGRATION_ENABLED_TEMPLATE = "org.eclipse.tractusx.edc.postgresql.migration.%s.enabled";
-
     private static final String DEFAULT_MIGRATION_SCHEMA = "public";
     @Setting(value = "Schema used for the migration", defaultValue = DEFAULT_MIGRATION_SCHEMA)
     private static final String MIGRATION_SCHEMA = "org.eclipse.tractusx.edc.postgresql.migration.schema";
+    private static final String PASSWORD = "password";
+    private static final String USER = "user";
+    private static final String URL = "url";
+
+
+    private Monitor monitor;
 
     @Override
     public String name() {
@@ -52,6 +64,7 @@ abstract class AbstractPostgresqlMigrationExtension implements ServiceExtension 
     @Override
     public void initialize(final ServiceExtensionContext context) {
         var config = context.getConfig();
+        monitor = context.getMonitor().withPrefix("Migration");
 
         var subSystemName = Objects.requireNonNull(getSubsystemName());
         var enabled = config.getBoolean(MIGRATION_ENABLED_TEMPLATE.formatted(subSystemName), Boolean.valueOf(DEFAULT_MIGRATION_ENABLED_TEMPLATE));
@@ -65,14 +78,31 @@ abstract class AbstractPostgresqlMigrationExtension implements ServiceExtension 
 
         var dataSourceName = datasourceConfig.getString("name", null);
         if (dataSourceName == null) {
-            context.getMonitor().warning("No 'name' setting in group %s found, no schema migrations will run for subsystem %s"
+            monitor.warning("No 'name' setting in group %s found, no schema migrations will run for subsystem %s"
                     .formatted(configGroup, subSystemName));
             return;
         }
 
-        var jdbcUrl = datasourceConfig.getString("url");
+        var rootPath = EDC_DATASOURCE_PREFIX + "." + datasourceConfig.currentNode();
+
+        var urlProperty = rootPath + "." + URL;
+        var jdbcUrl = ofNullable(getVault().resolveSecret(urlProperty)).orElseGet(readFromConfig(datasourceConfig, URL));
+
+        if (jdbcUrl == null) {
+            throw new EdcException("Mandatory config '%s' not found. Please provide a value for the '%s' property, either as a secret in the vault or an application property.".formatted(urlProperty, urlProperty));
+        }
+
+        var jdbcUser = ofNullable(getVault().resolveSecret(rootPath + "." + USER))
+                .orElseGet(readFromConfig(datasourceConfig, USER));
+        var jdbcPassword = ofNullable(getVault().resolveSecret(rootPath + "." + PASSWORD))
+                .orElseGet(readFromConfig(datasourceConfig, PASSWORD));
+
         var jdbcProperties = new Properties();
         jdbcProperties.putAll(datasourceConfig.getRelativeEntries());
+
+        // only set if not-null, otherwise Properties#add throws a NPE
+        ofNullable(jdbcUser).ifPresent(u -> jdbcProperties.put(USER, u));
+        ofNullable(jdbcPassword).ifPresent(p -> jdbcProperties.put(PASSWORD, p));
 
         var driverManagerConnectionFactory = new DriverManagerConnectionFactory();
         var dataSource = new ConnectionFactoryDataSource(driverManagerConnectionFactory, jdbcUrl, jdbcProperties);
@@ -88,6 +118,15 @@ abstract class AbstractPostgresqlMigrationExtension implements ServiceExtension 
         }
     }
 
+    protected abstract Vault getVault();
+
     protected abstract String getSubsystemName();
 
+    private @NotNull Supplier<@Nullable String> readFromConfig(Config config, String value) {
+        return () -> {
+            var entry = EDC_DATASOURCE_PREFIX + "." + config.currentNode() + "." + value;
+            monitor.warning("Database configuration value '%s' not found in vault, will fall back to Config. Please consider putting database configuration into the vault.".formatted(entry));
+            return config.getString(value, null);
+        };
+    }
 }
